776b0073dbbe8b928cdd19ecce3f6ac88582950a
Yogesh Kumar Chaurse <yogesh.chaurse@weboniselab.com>
2016-08-30 19:32:15 +0530

    added folders

---
+++ app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'webonise123#',
+		'database' => 'assignment_database',
+	);
+}
